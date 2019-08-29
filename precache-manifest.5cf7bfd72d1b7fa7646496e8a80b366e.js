f9bb05d7c36d2dbad02ac28f031e80a019420d67
Josh Grossman <grossmanjoshuaj@gmail.com>
2019-08-29 16:10:59 -0500

    Updates

---
+++ precache-manifest.5cf7bfd72d1b7fa7646496e8a80b366e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "487582dd8c382db7f1a027854a545a97",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "31a34a48322f20d40d8d",
+    "url": "/clicky-game/static/css/main.de7c97f4.chunk.css"
+  },
+  {
+    "revision": "c02afe3181c5d93d8299",
+    "url": "/clicky-game/static/js/2.86a6994e.chunk.js"
+  },
+  {
+    "revision": "31a34a48322f20d40d8d",
+    "url": "/clicky-game/static/js/main.97baaac1.chunk.js"
+  },
+  {
+    "revision": "79ea0d5502c3a6ac0f74",
+    "url": "/clicky-game/static/js/runtime~main.80bf01e5.js"
+  }
+]);
